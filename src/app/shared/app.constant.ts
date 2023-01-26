da71ce1cfda45f8ed1fd144820bce7ddd71d7f83
hsharma46 <hi.sharma2029@gmail.com>
2023-01-26 20:45:16 +0530

    api updated

--- src/app/shared/app.constant.ts
+++ src/app/shared/app.constant.ts
@@ -1,6 +1,7 @@
 export class AppConstant {
-    public static LOCAL_URL = 'http://localhost:4400/api/';
-    public static DEV_URL = 'https://bishwa-survey-api.herokuapp.com/api/';
+    public static LOCAL_URL = 'http://localhost:3000/api/';
+    // public static DEV_URL = 'https://bishwa-survey-api.herokuapp.com/api/';
+    public static DEV_URL = 'https://mysterious-cyan-jersey.cyclic.app/api/';
     public static API_URL = AppConstant.DEV_URL;
 }
 
